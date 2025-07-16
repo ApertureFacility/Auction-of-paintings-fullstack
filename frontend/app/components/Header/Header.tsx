29bdf3de9bbd6922f44f8ea8b2b8d222bcbcb9d3
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-07-16 13:44:58 +0300

    chore: code cleanup across multiple files

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./Header.module.css";
 import SearchInput from "../Search/Search";
 import { ChangeEvent } from "react";
@@ -6,7 +8,6 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>AUCTION.COM</div>
-
       <nav className={styles.nav}>
         <a href="#">preferred access</a>
         <a href="#">about</a>
@@ -14,26 +15,24 @@ const Header = () => {
         <a href="#">services</a>
         <a href="#">Как купить или продать</a>
       </nav>
-
       <div className={styles.right}>
         <div className={styles.rigt_part}>
-        <button className={styles.authButton}>
-          <img src="/login.svg" alt="auth" />
-          <span>Авторизация</span>
-        </button>
-
-
-        <img src="/lupa.svg" alt="Поиск" className={styles.icon} />
-        <img src="/star.svg" alt="Избранное" className={styles.iconStar} />
-
-
+          <button className={styles.authButton}>
+            <img src="/login.svg" alt="auth" />
+            <span>Авторизация</span>
+          </button>
+          <img src="/lupa.svg" alt="Поиск" className={styles.icon} />
+          <img src="/star.svg" alt="Избранное" className={styles.iconStar} />
         </div>
-      <img src="/Bag.svg" alt="Корзина" className={styles.iconBasket} />
-      <img src="/Burger.svg" alt="Избранное" className={styles.iconBurger} />
+        <img src="/Bag.svg" alt="Корзина" className={styles.iconBasket} />
+        <img src="/Burger.svg" alt="Избранное" className={styles.iconBurger} />
       </div>
-      <SearchInput  value={""} onChange={function (e: ChangeEvent<HTMLInputElement>): void {
-              throw new Error("Function not implemented.");
-          } }/>
+      <SearchInput
+        value={""}
+        onChange={function (e: ChangeEvent<HTMLInputElement>): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </header>
   );
 };
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,11 @@
-"use client"
-
-
-import styles from "./page.module.css";
-import Button from "./components/Button/Button";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-      <Button onClick={() => console.log('Primary clicked')}>
-    Черная кнопка
-</Button>
-
-<Button variant="secondary" onClick={() => console.log('Secondary clicked')}>
-    Белая кнопка с обводкой
-</Button>
-</main>
-    </div>
+    <section>
+    <h1>Главная страница</h1>
+    <p>Добро пожаловать!</p>
+  </section>
   );
 }
