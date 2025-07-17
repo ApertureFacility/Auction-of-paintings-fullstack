4e67d730ed54b16d8685d1a7c88acaef76c7ea2d
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-07-17 19:55:45 +0300

    feat: add env files and setup DB connection health check

---
+++ backend/app/api/test.py
@@ -0,0 +1,11 @@
+from sqlalchemy import text  # добавь импорт
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.db import get_db
+
+router = APIRouter()
+
+@router.get("/ping-db")
+async def ping_db(session: AsyncSession = Depends(get_db)):
+    result = await session.execute(text("SELECT 1"))  # оберни в text()
+    return {"db_status": result.scalar()}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from typing import Union
+from app.api import test
 
 from fastapi import FastAPI
 
 app = FastAPI()
-
+app.include_router(test.router)
 
 @app.get("/")
 def read_root():
