b30c0d48e5275d9d75ade0b2a5c750136e59a402
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-07-25 13:12:26 +0300

    feat(api): enable CORS for cross-origin requests

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,31 @@
 from typing import Union
 from .api import test 
-
+from .auth.router import router as auth_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .lots.routers import router as lots_router
+
 
 app = FastAPI()
+
+# Роутеры
 app.include_router(test.router)
+app.include_router(auth_router)
+app.include_router(lots_router)
+
+# CORS настройки
+origins = [
+    "http://localhost:3000",  # React frontend
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Разрешённые адреса
+    allow_credentials=True,
+    allow_methods=["*"],              # Разрешить все методы
+    allow_headers=["*"],              # Разрешить все заголовки
+)
 
 @app.get("/")
 def read_root():
