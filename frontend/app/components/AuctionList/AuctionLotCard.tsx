82335ecc29f13ed999c2768d0f301b2ec9e53a3f
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-08-01 21:06:31 +0300

    Frontend (refactor) replace interface into unterfaces directory

--- frontend/app/components/AuctionList/AuctionLotCard.tsx
+++ frontend/app/components/AuctionList/AuctionLotCard.tsx
@@ -3,66 +3,69 @@
 import React from "react";
 import styles from "./AuctionLotCard.module.css";
 import { useModalStore } from "../../lib/modalStore";
+import { AuctionLotCardProps } from "@/app/interfaces/ILot";
 
-interface Lot {
-  id: number;
-  image: string;
-  title: string;
-  author: string;
-  lotNumber: number;
-  price: string;
-}
 
-interface Props {
-  lot: Lot;
-}
 
-const AuctionLotCard: React.FC<Props> = ({ lot }) => {
+
+
+const AuctionLotCardSmall = ({ lot }: AuctionLotCardProps) => {
   const { open } = useModalStore();
 
   const handleZoomClick = (e: React.MouseEvent) => {
     e.stopPropagation();
-    if (lot.image) {
-      open("image-zoom", { imageUrl: lot.image });
+    if (lot.image_url) {
+      open("image-zoom", { imageUrl: lot.image_url });
     }
   };
 
   return (
     <div className={styles.card}>
       <div className={styles.imageWrapper}>
-        <img src={lot.image} alt={lot.title} className={styles.image} />
-
+        <img
+          src={lot.image_url}
+          alt={lot.title}
+          className={styles.image}
+          onError={(e) => {
+            (e.target as HTMLImageElement).src = "/placeholder-image.png";
+          }}
+        />
         <img
           src="/LupaCardIcon.png"
-          alt="zoom"
+          alt="Zoom"
           className={styles.iconZoom}
           onClick={handleZoomClick}
         />
-
         <img
           src="/AddFavCard2.png"
-          alt="favorite"
+          alt="Favorite"
           className={styles.iconStar}
         />
       </div>
 
       <div className={styles.authorRow}>
         <span className={styles.label}>АВТОР:</span>
-        <span className={styles.author}>{lot.author.toUpperCase()}</span>
+        <span className={styles.author}>{lot.author}</span>
       </div>
 
       <h2 className={styles.title}>{lot.title}</h2>
+
       <div className={styles.authorWrapper}>
-        <p className={styles.text}>Лот №{lot.lotNumber}</p>
+        <p className={styles.text}>Лот №{lot.id}</p>
       </div>
 
       <div className={styles.price_wrapper}>
-        <img src="/dollar.svg" alt="price_icon" className={styles.icon} />
+        <img src="/dollar.svg" alt="Price icon" className={styles.icon} />
         <span className={styles.label}>СТОИМОСТЬ:</span>
-        <span className={styles.price}>{lot.price.toUpperCase()} RUB</span>
+        <span className={styles.price}>
+          {typeof lot.current_price === "number"
+            ? lot.current_price.toFixed(2)
+            : lot.current_price}{" "}
+          RUB
+        </span>
       </div>
     </div>
   );
 };
 
-export default AuctionLotCard;
+export default AuctionLotCardSmall;
