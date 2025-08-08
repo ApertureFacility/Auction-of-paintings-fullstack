e08364f900517cdfc15a5b205ab1575bfe558b26
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-08-08 16:46:28 +0300

    Frontend (feat) added fav lots requests

--- frontend/app/apiRequests/userRequests.ts
+++ frontend/app/apiRequests/userRequests.ts
@@ -52,3 +52,81 @@ import { RegisterPayload } from "../interfaces/IUser"
     localStorage.setItem("access_token", data.access_token)
     return data
   }
+
+  export async function addLotToFavorites(lotId: number) {
+    const token = localStorage.getItem("access_token");
+    if (!token) {
+      throw new Error("Сначала войдите в аккаунт");
+    }
+  
+    const res = await fetch(`${API_URL}/favorites/add/${lotId}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    if (!res.ok) {
+      const data = await res.json();
+      throw new Error(data.detail || "Не удалось добавить в избранное");
+    }
+  
+    return true;
+  }
+
+
+
+export async function fetchCurrentUser() {
+
+  const token = typeof window !== 'undefined' ? localStorage.getItem("access_token") : null;
+  
+  if (!token) {
+    throw new Error("Authentication token not found");
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/users/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      credentials: 'include' 
+    });
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        localStorage.removeItem("access_token");
+      }
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+}
+
+export const removeLotFromFavorites = async (lotId: string) => {
+  const token = typeof window !== 'undefined' ? localStorage.getItem("access_token") : null;
+
+  if (!token) {
+    throw new Error("Сначала войдите в аккаунт");
+  }
+
+  const response = await fetch(`${API_URL}/favorites/remove/${lotId}`, {
+    method: "DELETE",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.detail || "Не удалось удалить лот из избранного");
+  }
+
+  return await response.json();
+};
+
