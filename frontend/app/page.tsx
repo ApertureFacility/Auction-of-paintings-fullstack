4959565973d745780474d5aec412e3592ecd95ad
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-08-10 12:12:30 +0300

    Frontend (feat) add home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,30 @@
 "use client";
-import { useState } from "react";
+import AuctionGrid from "./components/AuctionList/AuctionGrid";
+import FilterComponent from "./components/FilterSearchComponent/FilterComponent";
+import HiglightNews from "./components/News/HiglightNews";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <section>
-    <h1>Главная страница</h1>
-    <p>Добро пожаловать!</p>
-  </section>
+    <>
+      <span className={styles.headerText}>
+        Тайна затерянных сокровищ самого известного императора ацтеков —
+        Монтесумы
+      </span>
+      <img
+        src="/HeaderPic2.png"
+        alt="Header Background"
+        className={styles.headerImage}
+      />
+      <section>
+        <div className={styles.layout_container}>
+          <AuctionGrid />
+          <FilterComponent />
+        </div>
+        <div className={styles.margin_container}>
+          <HiglightNews />
+        </div>
+      </section>
+    </>
   );
 }
